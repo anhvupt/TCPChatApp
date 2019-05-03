cb106a2b943d7cd2243fd37ee2ad6e3c67c68b1d
anhvupt <anhvupt2412@gmail.com>
2019-05-03 20:38:43 +0700

    client server

---
+++ Client/Client/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Client
+{
+    public partial class Form1 : Form
+    {
+        ClientProgram client = new ClientProgram();
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+            client.SetDataFunction = new ClientProgram.SetDataControl(SetData);
+        }
+        private void SetData(string Data)
+        {
+            this.listBox1.Items.Add(Data);
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            client.Connect(IPAddress.Parse(this.textBox1.Text), int.Parse(this.textBox2.Text));
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            client.Disconnect();
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            client.SendData(this.richTextBox1.Text);
+            this.richTextBox1.Text = "";
+        }
+    }
+}
